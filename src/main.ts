0f2eb101345feae055a6ab331b666ce57710e928
JYN <hellodbsk01@gmail.com>
2025-06-05 18:21:03 +0900

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
+      'http://localhost:3000',
       'https://portfolio-front-nu9h-git-master-jeon-yunas-projects.vercel.app',
     ],
 
