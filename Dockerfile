e9407452e23e4f949a67da25b462f3a34162b10d
JYN <hellodbsk01@gmail.com>
2025-06-05 18:20:01 +0900

    main

--- Dockerfile
+++ Dockerfile
@@ -8,15 +8,11 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install
 
-
 # Copy app source
 COPY . .
-
-# NestJS 앱 빌드
-RUN npm run build
 
 # Open port
 EXPOSE 3002
 
 # Default command
-CMD ["node", "dist/main"]
+CMD ["npm", "run", "start:dev"]
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true, //모든출처 허용
+    origin: [
+      'https://portfolio-front-nu9h-git-master-jeon-yunas-projects.vercel.app',
+    ],
 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     Credentials: true,
